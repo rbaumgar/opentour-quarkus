040ce9de0bdbdccbd35e281a6abab4bbc45e816c
Robert Baumgartner <rbaumgar@redhat.com>
2024-01-15 16:54:21 +0100

    kubernetes

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,7 @@
 package org.acme;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +10,12 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "stage", defaultValue = "local")
+    String stage;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "Hello RESTEasy " + stage;
     }
 }
